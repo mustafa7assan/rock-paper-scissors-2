e8628e8d76848fbe1cd350291e90c9469c843a86
Mustafa Hassan <mustafahassan.dev@gmail.com>
2024-12-03 06:00:26 +0200

    Add functions to generate a random choice for computer

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("Hello, World!");
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 3) + 1;
+};
+
+const getComputerChoice = () => {
+  const randomNumber = generateRandomNumber();
+  switch (randomNumber) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+    default:
+      return "invalid choice";
+  }
+};
