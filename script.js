55cf8eea9617a0487d99b60dafc6dcf0b8811884
Mustafa Hassan <mustafahassan.dev@gmail.com>
2024-12-03 06:48:17 +0200

    Add playRound function to determine round winner and update score

--- script.js
+++ script.js
@@ -21,13 +21,43 @@ const getComputerChoice = () => {
 const getHumanChoice = () => {
   let userInput;
   while (true) {
-    userInput = prompt("Please enter your choice (rock, paper or scissor)")
+    userInput = prompt("Please enter your choice (rock, paper or scissors)")
       .trim()
       .toLowerCase();
-    if (["rock", "paper", "scissor"].includes(userInput)) {
+    if (["rock", "paper", "scissors"].includes(userInput)) {
       return userInput;
     } else {
       alert("Invalid choice. Please try again with rock, paper, or scissors.");
     }
   }
 };
+
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === computerChoice) {
+    console.log(`Tie Game`);
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    console.log(`You won! Rock beats Scissors`);
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    console.log(`You won! Paper beats Rock`);
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    console.log(`You won! Scissors beats Paper`);
+  } else {
+    computerScore++;
+    console.log(
+      `You Lose! ${
+        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      } beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`
+    );
+  }
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
